2ce617145281e9f598a3ea3c62292f8b891c2b93
Hector Toro <alejotoro94@hotmail.com>
2022-02-18 14:58:39 -0500

    Template: Servicios país - obtener información de paises

---
+++ src/app/services/pais.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  constructor( private http: HttpClient ) { }
+
+  getPaises() {
+
+    return this.http.get('https://restcountries.com/v3.1/lang/spa')
+                      .pipe( 
+                        map( (resp: any = [] ) => {
+                          return resp.map( (pais : {name: any; alpha3Code: any;}) => {
+                             return {
+                               nombre: pais.name,
+                               codigo: pais.alpha3Code
+                             }
+                          })
+                        })
+                       );
+
+  }
+}
