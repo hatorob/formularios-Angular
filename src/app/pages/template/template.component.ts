e4ac03279f055f2b13d0a01c5e5bd3e01048c18f
Hector Toro <alejotoro94@hotmail.com>
2022-02-18 11:20:47 -0500

    Template: Obteniendo la información del formulario

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -6,13 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template.component.css']
 })
 export class TemplateComponent implements OnInit {
+  
+  usuario = {
+    nombre: 'Carlos', //establecer un valor por defecto al formulario
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  guardar() {
-    console.log("Disparado");
+  guardar( forma: NgForm ) {
+    console.log("forma", forma.value);
   }
 }
