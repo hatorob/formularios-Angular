ff4a3912d9ca140949c5935bdfb4b0b231e3deb4
Hector Toro <alejotoro94@hotmail.com>
2022-02-21 17:26:19 -0500

    Reactivo: validaciones personalizadas

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -11,7 +12,7 @@ export class ReactiveComponent implements OnInit {
   forma!: FormGroup;
   
 
-  constructor( private fb: FormBuilder) { 
+  constructor( private fb: FormBuilder, private validadores: ValidadoresService) { 
     // Se recomienda inicializar el formgroup desde el constructor
     this.crearFormulario();
     this.cargarDataAlFormulario();
@@ -43,7 +44,7 @@ export class ReactiveComponent implements OnInit {
   crearFormulario() {
     this.forma = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(5)]],
-      apellido: ['', [Validators.required, Validators.minLength(2)]],
+      apellido: ['', [Validators.required, Validators.minLength(2), this.validadores.noHerrera]],
       correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       direccion: this.fb.group({
         distrito: ['', Validators.required],
